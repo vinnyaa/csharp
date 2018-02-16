e9328a4aff5574ef7aae8ffe3d00215f6a3b5273
Vince <stokar2014@gmail.com>
2018-02-15 22:53:00 -0500

    textchanged and new method added to print something

--- serial.cs
+++ serial.cs
@@ -5,10 +5,15 @@ namespace CSSerialLibrary {
 
 		public static void Main(String[] args) {
 			dothings();
+			dothings2();
 		}
 
 		public static void dothings() {
-			Console.WriteLine("This is the first github managed file system");
+			Console.WriteLine("this branch should have text changed and new method");
+		}
+
+		public static void dothings2() {
+			Console.WriteLine("Dothings2");
 		}
 	}
 }

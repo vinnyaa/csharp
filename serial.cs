80fb7b40599311deb06682d8cd46918493b70314
Vince <stokar2014@gmail.com>
2018-02-15 22:14:02 -0500

    Changed print into a seperate function that main now calls

--- serial.cs
+++ serial.cs
@@ -4,8 +4,11 @@ namespace CSSerialLibrary {
 	public class SerialWork {
 
 		public static void Main(String[] args) {
+			dothings();
+		}
+
+		public static void dothings() {
 			Console.WriteLine("This is the first github managed file system");
-			
 		}
 	}
 }

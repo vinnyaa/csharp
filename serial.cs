ea145d112f4e1c95b96b458fdf035c2326e82631
Vince <stokar2014@gmail.com>
2018-02-15 20:48:04 -0500

    This has just a print statement. Just trying to get into the habit of committing work to github

---
+++ serial.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace CSSerialLibrary {
+	public class SerialWork {
+
+		public static void Main(String[] args) {
+			Console.WriteLine("This is the first github managed file system");
+		}
+	}
+}
